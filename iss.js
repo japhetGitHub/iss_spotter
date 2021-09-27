915fd118cbb4923f3c78c5cd42e8c6641f68b559
Japhet Elias <japhet9@gmail.com>
2021-09-27 02:38:00 +0000

    add checks for potential server errors from http call

--- iss.js
+++ iss.js
@@ -9,12 +9,23 @@
 
 const request = require('request');
 
-const fetchMyIP = function(callback) {
+const fetchMyIP = function (callback) {
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org?format=json', (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
     if (error) {
       callback(error, null);
+      return;
     }
+    
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
     const ip = JSON.parse(body).ip;
 
     callback(null, ip);
